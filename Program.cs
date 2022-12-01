58b1acc19a327be453e333e0e8bfe545c4b02552
if220112 <116262854+if220112@users.noreply.github.com>
2022-12-01 11:09:18 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿System.Console.WriteLine("Please enter a number: ");
+int number1 = int.Parse(Console.ReadLine()!);
+System.Console.WriteLine("Please enter a second number: ");
+int number2 = int.Parse(Console.ReadLine()!);
+System.Console.WriteLine("Please enter the result of the two numbers: ");
+int result = int.Parse(Console.ReadLine()!);
+
+
+
+ if (number1 * number2 == result || number2 * number1 == result)
+{
+System.Console.WriteLine("The two numbers are multiplied");
+}
+
+else if (number1 + number2 == result || number2 + number1 == result)
+{
+System.Console.WriteLine("The two numbers are added ");
+}
+
+else if (number1 / number2 == result || number2 / number1 == result)
+{
+System.Console.WriteLine("The two numbers are divided ");
+}
+
+else if (number1 - number2 == result || number2 - number1 == result)
+{
+System.Console.WriteLine("The two numbers are subtracted ");
+}
+
+
+
